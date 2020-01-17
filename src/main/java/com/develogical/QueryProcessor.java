51ad7caa66cda8af9bd4b9534ad1b9e7c66b60c2
Cheryl  Chen <cheryl.chen16@imperial.ac.uk>
2020-01-17 21:10:43 +0000

    test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,7 +9,7 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
         if (query.toLowerCase().contains("chicken")) {
-            return "delish delis;
+            return "delish delis";
         }
         return "";
     }
