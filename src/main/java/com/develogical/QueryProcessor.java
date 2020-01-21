2d997b672a70cf48334ad1163886c89e9b6e7aed
Cheryl  Chen <cheryl.chen16@imperial.ac.uk>
2020-01-21 11:18:33 +0000

    test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,8 +1,13 @@
 package com.develogical;
 
+import java.util.Arrays;
+
 public class QueryProcessor {
 
     public String process(String query) {
+        String[] splitQuery = query.split(" ,./?");
+        splitQuery = (String[]) Arrays.stream(splitQuery).map(String::toLowerCase).toArray();
+        String[] teamNameOutput = {"what", "is", "your", "team", "name"};
         if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
@@ -11,7 +16,9 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("chicken")) {
             return "delish delis bok";
         }
-        if (query.toLowerCase().contains("team") && query.toLowerCase().contains("name")) {
+
+
+        if (Arrays.asList(splitQuery).containsAll(Arrays.asList(teamNameOutput))) {
             return "chicken";
         }
         return "";
